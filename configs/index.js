8ea43e7bfef734c97d078a121d395c7f1a3d7494
Sabari Rajan <sabarirajan.mg@gmail.com>
2022-05-24 13:26:15 +0530

    Adding oauth config placeholders

---
+++ configs/index.js
@@ -0,0 +1,13 @@
+// check the configs at twitter dev portal or private repo in bitbuclet https://bitbucket.org/sabarirajan_m/twitter-followers
+
+const configs = {
+  twitter: {
+    consumer_key: '',
+    consumer_secret_key: '',
+    access_token: '',
+    access_token_secret: '',
+    oauth_callback: 'http://127.0.0.1:5000/callback',
+  },
+};
+
+export default configs;
